6b2b2977d598d4a8d8e78a95546d8e861aed57a2
Funnyyanne <lochienpin@gmail.com>
2023-11-17 11:41:08 +0800

    个人页面，正在写登录相关的

--- app/src/main/java/com/anne/play/ui/page/mine/ProfilePage.kt
+++ app/src/main/java/com/anne/play/ui/page/mine/ProfilePage.kt
@@ -16,7 +16,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -98,7 +97,7 @@ fun UserInfoFields(
         ProfileProperty(
             ArticleModel(
                 title = stringResource(R.string.my_github),
-                link = "https://github.com/zhujiang521",
+                link = "https://github.com/Funnyyanne/PlayAndroid",
             ),
             enterArticle,
         )
