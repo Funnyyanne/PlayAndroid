e3e98be40c146669b8be300ad6af2030bb4f4426
Funnyyanne <lochienpin@gmail.com>
2023-11-17 11:37:07 +0800

    1.Project的ArticleListPage和OfficialAccountPage 后面要整合

--- app/src/main/java/com/anne/play/ui/main/MainPage.kt
+++ app/src/main/java/com/anne/play/ui/main/MainPage.kt
@@ -1,8 +1,8 @@
 package com.anne.play.ui.main
 
+import android.content.res.Configuration
 import androidx.annotation.StringRes
 import androidx.compose.animation.Crossfade
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
@@ -22,16 +22,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import androidx.paging.ExperimentalPagingApi
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.anne.play.R
+import com.anne.play.logic.model.PlayState
 import com.anne.play.ui.page.article.OfficialAccountPage
 import com.anne.play.ui.page.home.HomePage
-import com.anne.play.ui.page.mine.Mine
+import com.anne.play.ui.page.login.LoginViewModel
+import com.anne.play.ui.page.login.LogoutDefault
 import com.anne.play.ui.page.mine.ProfilePage
+import com.anne.play.ui.page.project.ArticleListPage
+import com.anne.play.ui.page.project.ProjectViewModel
 import java.util.Locale
 
 /**
@@ -44,7 +50,7 @@ import java.util.Locale
 @Composable
 fun MainPage(
     actions: PlayActions,
-    viewModel: HomeViewModel = viewModel()
+    viewModel: HomeViewModel = viewModel(),
 ) {
     val navController = rememberNavController()
     val tabs = CourseTabs.values()
@@ -59,25 +65,60 @@ fun MainPage(
                     icon = { Icon(painterResource(id = tab.ordinal), contentDescription = null) },
                     selected = tab == position,
                     onClick = { viewModel.onPositionChanged(tab) },
-                    label = { Text(text = stringResource(id = tab.title).toUpperCase(Locale.ROOT)) }
+                    label = { Text(text = stringResource(id = tab.title).uppercase(Locale.ROOT)) },
                 )
             }
-
         }
     }) { innerPadding ->
         val modifier = Modifier.padding(innerPadding)
+        val isLand = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
+
         Crossfade(targetState = position, label = "") { screen ->
             when (screen) {
                 CourseTabs.HOME_PAGE -> HomePage(actions, modifier)
-                CourseTabs.PROJECT -> ProfilePage(actions.enterArticle)
+                CourseTabs.PROJECT -> {
+                    val projectViewModel = viewModel<ProjectViewModel>()
+                    val lazyPagingItem = projectViewModel.articleResult.collectAsLazyPagingItems()
+                    val tree by projectViewModel.treeLiveData.observeAsState(PlayState.PlayLoading)
+                    val treePosition by projectViewModel.position.observeAsState(0)
+                    ArticleListPage(
+                        modifier,
+                        tree,
+                        treePosition,
+                        lazyPagingItem,
+                        {
+                            projectViewModel.getDataList()
+                        },
+                        {
+                            projectViewModel.searchArticle(it)
+                        },
+                        {
+                            projectViewModel.onPositionChanged(it)
+                        },
+                    ) {
+                        actions.enterArticle(it)
+                    }
+                }
+
                 CourseTabs.OFFICIAL_ACCOUNT -> OfficialAccountPage(actions.enterArticle, modifier)
-                CourseTabs.MINE -> Mine(actions)
+                CourseTabs.MINE -> {
+                    val viewModel: LoginViewModel = viewModel()
+                    val logoutState by viewModel.logoutState.observeAsState(LogoutDefault)
+                    ProfilePage(
+                        modifier,
+                        isLand = isLand,
+                        actions.toLogin,
+                        logoutState,
+                        { viewModel.logout() },
+                    ) {
+                        actions.enterArticle(it)
+                    }
+                }
+
                 else -> {}
             }
-
         }
     }
-
 }
 
 enum class CourseTabs(@StringRes val title: Int, val icon: ImageVector) {
@@ -86,6 +127,4 @@ enum class CourseTabs(@StringRes val title: Int, val icon: ImageVector) {
     PROJECT(R.string.project, Icons.Filled.Info),
     OFFICIAL_ACCOUNT(R.string.official_account, Icons.Filled.DateRange),
     MINE(R.string.mine, Icons.Default.AccountCircle),
-
-
 }
